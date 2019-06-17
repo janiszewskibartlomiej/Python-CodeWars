85244cd1c320fd3b12b2d9a439e69f6a4d63d8d9
Bartlomiej Janiszewski <43078086+janiszewskibartlomiej@users.noreply.github.com>
2019-06-17 19:14:21 +0200

    updata test

--- 8_kyu/Total_amount_of_points/test_total_amount.py
+++ 8_kyu/Total_amount_of_points/test_total_amount.py
@@ -3,13 +3,16 @@ from points import points
 
 
 class TestChampion(unittest.TestCase):
-    def champion_results(self):
-        
-        self.assertEqual(points(['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3']), 30)
-        self.assertEqual(points(['1:1', '2:2', '3:3', '4:4', '2:2', '3:3', '4:4', '3:3', '4:4', '4:4']), 10)
-        self.assertEqual(points(['0:1', '0:2', '0:3', '0:4', '1:2', '1:3', '1:4', '2:3', '2:4', '3:4']), 0)
-        self.assertEqual(points(['1:0', '2:0', '3:0', '4:0', '2:1', '1:3', '1:4', '2:3', '2:4', '3:4']), 15)
-        self.assertEqual(points(['1:0', '2:0', '3:0', '4:4', '2:2', '3:3', '1:4', '2:3', '2:4', '3:4']), 12)
+    def test_result_1(self):
+        self.assertEqual(points((['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3'])), 30)
+    def test_result_2(self):
+        self.assertEqual(points((['1:1', '2:2', '3:3', '4:4', '2:2', '3:3', '4:4', '3:3', '4:4', '4:4'])), 10)
+    def test_result_3(self):
+        self.assertEqual(points((['0:1', '0:2', '0:3', '0:4', '1:2', '1:3', '1:4', '2:3', '2:4', '3:4'])), 0)
+    def test_result_4(self):
+        self.assertEqual(points((['1:0', '2:0', '3:0', '4:0', '2:1', '1:3', '1:4', '2:3', '2:4', '3:4'])), 15)
+    def test_result_5(self):
+        self.assertEqual(points((['1:0', '2:0', '3:0', '4:4', '2:2', '3:3', '1:4', '2:3', '2:4', '3:4'])), 12)
 
 
 if __name__ == '__main__':
