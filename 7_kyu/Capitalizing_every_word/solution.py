f2d9da78f736d9156bfdb2e196456dfee908ec89
Bartlomiej Janiszewski <43078086+janiszewskibartlomiej@users.noreply.github.com>
2019-07-04 16:40:02 +0200

    upload

--- 7_kyu/Capitalizing_every_word/solution.py
+++ 7_kyu/Capitalizing_every_word/solution.py
@@ -10,6 +10,14 @@ def toJadenCase(string):
     for word in string_list:
         xx += word + ' '
     return xx.strip()
+-----------------------------------------------
+# def toJadenCase(string):
+#   return ' '.join(x.capitalize() for x in string.split())
+------------------------------------------------
+# import string
+# def toJadenCase(string1):
+#     return string.capwords(string1)
+
 
 if __name__ == '__main__':
     quote = "How can mirrors be real if our eyes aren't real"
