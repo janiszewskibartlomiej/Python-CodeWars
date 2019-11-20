af08066a17ddebd87e472fa52d16ea2ea1b84b45
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-11-20 22:06:53 +0100

    Exercise return diff in two array - difference method by set

--- 6_kyu/ArrayDiff/ArrayDiff.py
+++ 6_kyu/ArrayDiff/ArrayDiff.py
@@ -5,9 +5,12 @@ def array_diff(a, b):
     #         nowe.append(i)
     # return nowe
 
-    new_array = [x for x in a if x not in b]
-    return new_array
+    # new_array = [x for x in a if x not in b]
+    # return new_array
 
+    a, b = set(a), set(b)
+    x = a.difference(b)
+    return list(x)
 
 x = array_diff([1, 2], [1])
 print(x)
