7742cdb49d074cfbacd65c5c515a0f38cec6d21e
Bartłomiej Janiszewski <janiszewski.bartlomiej@gmail.com>
2020-01-10 13:43:07 +0100

    I added ex "Add or substract"

---
+++ 7_kyu/AddorSubtract/SolutionAddOrSubtract.py
@@ -0,0 +1,31 @@
+def calculate(s):
+    suma = 0
+    zz = test.replace('plus', '+')
+    print(zz)
+    zz = zz.replace('minus', '-')
+    for el in zz:
+        print(el)
+        if el.isnumeric():
+            suma += el
+
+
+    return suma
+
+
+
+# x = calculate("1plus2minus3plus4")
+# print(x)
+
+test = "1plus2minus3plus4"
+
+# print(type(int(test[0])))
+
+
+
+print(zz)
+
+print(int(zz))
+
+z = test[0]
+
+# print(test.split(int))
