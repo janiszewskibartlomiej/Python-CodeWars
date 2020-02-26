e7d8d4fca7bcfd5bf33199d3bc732beb5f642318
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2020-02-26 13:13:03 +0100

    Modyfication of exercises - Anstra

--- 7_kyu/GrowingPlant/SolutionGrowingPlant.py
+++ 7_kyu/GrowingPlant/SolutionGrowingPlant.py
@@ -1,13 +1,27 @@
 def growing_plant(upSpeed, downSpeed, desiredHeight):
     height = 0
     if 5 <= upSpeed <= 100:
-        height -= downSpeed
-    if 2 <= downSpeed < upSpeed:
         height += upSpeed
+    if 2 <= downSpeed < upSpeed:
+        height -= downSpeed
     if 4 <= desiredHeight <= 1000:
-         suma = height / desiredHeight
-    return round(suma)
+         suma = desiredHeight / height
+    if desiredHeight <= upSpeed:
+        suma = 1
+    return int(suma), height, suma
 
-
-x = growing_plant(100,10,910)
-print(x)
+if __name__ == '__main__':
+    x = growing_plant(100,10,910)
+    y = growing_plant(10,9,4)
+    z = growing_plant(5,2,0)
+    w = growing_plant(5,2,5)
+    wz = growing_plant(55,32,72)
+    print(x)
+    print("-------------------")
+    print(y)
+    print("-----------")
+    print(z)
+    print("==============")
+    print(w)
+    print("=================")
+    print(wz)
--- 7_kyu/GrowingPlant/TestGrowingPlant.py
+++ 7_kyu/GrowingPlant/TestGrowingPlant.py
@@ -3,12 +3,16 @@ from SolutionGrowingPlant import growing_plant
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEquals(growing_plant(100,10,910),10)
-        self.assertEquals(growing_plant(10,9,4),1)
-        self.assertEquals(growing_plant(5,2,0),1)
-        self.assertEquals(growing_plant(5,2,5),1)
-        self.assertEquals(growing_plant(5,2,6),2)
+    def test1(self):
+        self.assertEqual(growing_plant(100,10,910),10)
+    def test2(self):
+        self.assertEqual(growing_plant(10,9,4),1)
+    def test3(self):
+        self.assertEqual(growing_plant(5,2,0),1)
+    def test4(self):
+        self.assertEqual(growing_plant(5,2,5),1)
+    def test5(self):
+        self.assertEqual(growing_plant(5,2,6),2)
 
 
 if __name__ == '__main__':
